5072af35acb6fd97d1a01fbed83755db459aef48
patsul12 <patsul12@gmail.com>
2016-01-22 08:21:56 -0800

    add Stylist functionality and tests to the Client class

--- lib/client.rb
+++ lib/client.rb
@@ -1,3 +1,38 @@
 class Client
   attr_reader :id, :name
+
+  def initialize(attributes)
+    @name = attributes[:name]
+  end
+
+  def save
+    DB.exec("INSERT INTO clients (name) VALUES ('#{self.name}');")
+  end
+
+  def ==(other_client)
+    self.name == other_client.name
+  end
+
+  def self.find(name)
+    returned_clients = DB.exec("SELECT * FROM clients WHERE name = '#{name}';")
+    clients = []
+    returned_clients.each do |client|
+      name = client["name"]
+      temp_client = Stylist.new({name: name})
+      clients << temp_client
+    end
+    clients
+  end
+
+  def self.all
+    returned_clients = DB.exec("SELECT * FROM clients;")
+    clients = []
+    returned_clients.each do |client|
+      name = client["name"]
+      temp_client = Stylist.new({name: name})
+      clients << temp_client
+    end
+    clients
+  end
+
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,1 +1,31 @@
 require 'spec_helper'
+
+describe Client do
+  describe '.all' do
+    it 'is empty initially' do
+      expect(Client.all).to(eq([]))
+    end
+
+    it 'returns an array of all stylists in the database' do
+      test_client = Client.new({name: 'Franz'})
+      test_client.save
+      expect(Client.all).to(eq([test_client]))
+    end
+  end
+
+  describe '#find' do
+    it 'finds a stylist(s) in the database' do
+      test_client = Client.new({name: 'Franz'})
+      test_client.save
+      expect(Client.find("Franz")).to(eq([test_client]))
+    end
+  end
+
+  describe '#==' do
+    it 'returns true if two object share the same properties' do
+      test_client = Client.new({name: 'Franz'})
+      test_client2 = Client.new({name: 'Franz'})
+      expect(test_client).to(eq(test_client2))
+    end
+  end
+end
