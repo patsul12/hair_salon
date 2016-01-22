c1bd561360815d88bece20e92bff9e792d4415ec
patsul12 <patsul12@gmail.com>
2016-01-22 08:13:29 -0800

    add the all method to the stylist class for returning all stylists currently in the database.

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -5,7 +5,23 @@ class Stylist
     @name = attributes[:name]
   end
 
-  def self.all
-    []
+  def save
+    DB.exec("INSERT INTO stylists (name) VALUES ('#{self.name}');")
   end
+
+  def ==(other_stylist)
+    self.name == other_stylist.name
+  end
+
+  def self.all
+    returned_stylists = DB.exec("SELECT * FROM stylists;")
+    stylists = []
+    returned_stylists.each do |stylist|
+      name = stylist["name"]
+      temp_stylist = Stylist.new({name: name})
+      stylists << temp_stylist
+    end
+    stylists
+  end
+
 end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -5,5 +5,11 @@ describe Stylist do
     it 'is empty initially' do
       expect(Stylist.all).to(eq([]))
     end
+
+    it 'returns an array of all stylists in the database' do
+      test_stylist = Stylist.new({name: 'Franz'})
+      test_stylist.save
+      expect(Stylist.all).to(eq([test_stylist]))
+    end
   end
 end
